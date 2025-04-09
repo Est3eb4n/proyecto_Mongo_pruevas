38b9672ae4ff97ee801e5eef369615e04320d25b
Est3eb4n <119062636+Est3eb4n@users.noreply.github.com>
2025-04-09 08:54:24 -0500

    Add files via upload

--- commands.js
+++ commands.js
@@ -13,8 +13,8 @@ db.createCollection("inscriptionsDB")
 //|                                                                                                              |
 //| se usa el comando "use" pata crear la coleccion "Actme-School-Proyect"                                       |
 //|______________________________________________________________________________________________________________|
-use Actme-School-Proyect
-use Server_pruevas
+// use Actme-School-Proyect
+// use Server_pruevas
 //________________________________________________________________________________________________________________
 
 //________________________________________________________________________________________________________________
@@ -38,12 +38,13 @@ function createCollectionSchool(){
    });
    return"Operacion completada"
 }
+createCollectionSchool()
 //________________________________________________________________________________________________________________
 
 // COMANDOS DE VALIDACION
 
 // Colección cities
-db.createCollection("cities", {
+db.createCollection("CitiesDB", {
     validator: {
       $jsonSchema: {
         bsonType: "object",
@@ -56,9 +57,10 @@ db.createCollection("cities", {
       }
     }
   });
-  
+//________________________________________________________________________________________________________________
+
   // Colección teachers
-  db.createCollection("teachers", {
+  db.createCollection("TeachersDB", {
     validator: {
       $jsonSchema: {
         bsonType: "object",
@@ -81,9 +83,10 @@ db.createCollection("cities", {
       }
     }
   });
-  
+//________________________________________________________________________________________________________________
+
   // Colección topics
-  db.createCollection("topics", {
+  db.createCollection("TopicsDB", {
     validator: {
       $jsonSchema: {
         bsonType: "object",
@@ -97,7 +100,8 @@ db.createCollection("cities", {
       }
     }
   });
-  
+//________________________________________________________________________________________________________________
+
   // Colección courses
   db.createCollection("courses", {
     validator: {
@@ -144,9 +148,10 @@ db.createCollection("cities", {
       }
     }
   });
-  
+//________________________________________________________________________________________________________________
+
   // Colección students
-  db.createCollection("students", {
+  db.createCollection("StudentsDB", {
     validator: {
       $jsonSchema: {
         bsonType: "object",
@@ -179,9 +184,10 @@ db.createCollection("cities", {
       }
     }
   });
+//________________________________________________________________________________________________________________
 
   // Colección Inscription
-  db.createCollection("inscriptions", {
+  db.createCollection("inscriptionsDB", {
     validator: {
       $jsonSchema: {
         bsonType: "object",
@@ -201,26 +207,375 @@ db.createCollection("cities", {
       }
     }
   });
+//________________________________________________________________________________________________________________
 
 // COMANDOS PARA INDEXACION
 // Índices para courses
 db.courses.createIndex({ code: 1 }, { unique: true });
 db.courses.createIndex({ "dateRange.startDate": 1, "dateRange.endDate": 1 });
 db.courses.createIndex({ teacherIds: 1 });
+//________________________________________________________________________________________________________________
 
 // Índices para teachers
 db.teachers.createIndex({ code: 1 }, { unique: true });
 db.teachers.createIndex({ "identification.number": 1 }, { unique: true });
 db.teachers.createIndex({ email: 1 }, { unique: true });
+//________________________________________________________________________________________________________________
 
 // Índices para students
 db.students.createIndex({ code: 1 }, { unique: true });
 db.students.createIndex({ "identification.number": 1 }, { unique: true });
 db.students.createIndex({ email: 1 }, { unique: true });
 db.students.createIndex({ "contact.cityId": 1 });
+//________________________________________________________________________________________________________________
 
 // Índices para inscriptions
 db.inscriptions.createIndex({ studentId: 1 });
 db.inscriptions.createIndex({ courseId: 1 });
 db.inscriptions.createIndex({ studentId: 1, courseId: 1 }, { unique: true });
+//________________________________________________________________________________________________________________
 
+// COMANDOS PARA LAS AGREGACIONES
+
+// AGRAGACION PARA FILTAR POR FECHAS LA CUALES MUENTRAN datos del curso, profesor asignado, aula, cantidad, Estudiantes
+db.CoursesDB.aggregate([
+  {
+    $match: {// filtra por los parametros que se le pasen en este caso por las fechas
+      "dateRange.startDate": { $gte: "01/05/2025" },
+      "dateRange.endDate": { $lte: "30/09/2025" }
+    }
+  },
+  {
+    $lookup: {// toma los documentos de la coleccion actual
+      from: "TeachersDB",// se le indica la coleccion donde se va a realizar la busqueda
+      localField: "teacherIds",// se pasa el campo de la coleccion actual que se desa copara
+      foreignField: "_id", // se le pasa el campo de la coleccion externa para compara
+      as: "teachers"// se pasa el nombre del nuevo array donde se pondra el resultado
+    }
+  },
+  {
+    $lookup: {// toma los documentos de la coleccion actual
+      from: "inscriptionsDB",// se le indica la coleccion donde se va a realizar la busqueda
+      localField: "_id",// se pasa el campo de la coleccion actual que se desa copara
+      foreignField: "courseId",// se le pasa el campo de la coleccion externa para compara
+      as: "enrollments" // se pasa el nombre del nuevo array donde se pondra el resultado
+    }
+  },
+  {
+    $addFields: {// se usa el operador sddfild para agregar los campos studentCount y classroomInfo
+      studentCount: { $size: "$enrollments" },// en las expreciones de pasa los parametros enrollments para mostrar la cantidad de estudiantes
+      classroomInfo: "$classRoom"// en las expreciones de pasa los parametros classRoom para mostrar la informacion del salon
+    }
+  },
+  {
+    $project: {
+      "enrollments": 0,
+      "teacherIds": 0
+    }
+  }
+]);
+//________________________________________________________________________________________________________________
+
+// Agregacion para mostara los datos del curso y su contenido tematico usando el code de curso
+db.CoursesDB.aggregate([
+  {
+    $match: {// filtra por los parametros que se le pasen en este caso por code de curso o por objectID
+      code: "CR-2001" 
+    }
+  },
+  {
+    $lookup: {// toma los documentos de la coleccion actual
+      from: "topics",// se le indica la coleccion donde se va a realizar la busqueda
+      localField: "topicId",// se pasa el campo de la coleccion actual que se desa copara
+      foreignField: "_id",// se le pasa el campo de la coleccion externa para compara
+      as: "topicDetails"// se pasa el nombre del nuevo array donde se pondra el resultado
+    }
+  },
+  {
+    $unwind: "$topicDetails"// se em plea el operador unwind para desanidar el array
+  },
+  {
+    $project: {// se usa el operador $project para indidar los parametros para mostrar
+      "code": 1,
+      "name": 1,
+      "description": 1,
+      "details": 1,
+      "topicDetails": 1,
+      "classRoom": 1
+    }
+  }
+]);
+
+//________________________________________________________________________________________________________________
+
+
+db.StudentsDB.aggregate([
+  {
+    $lookup: {
+      from: "CitiesDB",
+      localField: "contact.cityId",
+      foreignField: "_id",
+      as: "cityInfo"
+    }
+  },
+  {
+    $unwind: "$cityInfo"
+  },
+  {
+    $group: {
+      _id: "$cityInfo.city",
+      students: {
+        $push: {
+          code: "$code",
+          firstName: "$firstName",
+          lastName: "$lastName",
+          gender: "$gender",
+          age: {
+            $dateDiff: {
+              startDate: { $toDate: "$birthdate" },
+              unit: "year"
+            }
+          },
+          email: "$contact.email"
+        }
+      },
+      count: { $sum: 1 }
+    }
+  },
+  {
+    $sort: {
+      "_id": 1
+    }
+  }
+]);
+//________________________________________________________________________________________________________________
+
+
+db.inscriptionsDB.aggregate([
+  {
+    $match: {
+      studentId: ObjectId("65f8d3a7c1b8e8a9d4e5f301") // O usar código de estudiante
+    }
+  },
+  {
+    $lookup: {
+      from: "courses",
+      localField: "courseId",
+      foreignField: "_id",
+      as: "courseInfo"
+    }
+  },
+  {
+    $unwind: "$courseInfo"
+  },
+  {
+    $project: {
+      "courseCode": "$courseInfo.code",
+      "courseName": "$courseInfo.name",
+      "credits": "$courseInfo.details.credits",
+      "score": "$rate.score",
+      "comments": "$rate.comments"
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      courses: { $push: "$$ROOT" },
+      weightedSum: {
+        $sum: {
+          $multiply: ["$rate.score", "$courseInfo.details.credits"]
+        }
+      },
+      totalCredits: { $sum: "$courseInfo.details.credits" }
+    }
+  },
+  {
+    $addFields: {
+      weightedAverage: {
+        $divide: ["$weightedSum", "$totalCredits"]
+      }
+    }
+  }
+]);
+//________________________________________________________________________________________________________________
+
+
+db.inscriptionsDB.aggregate([
+  {
+    $match: {
+      courseId: ObjectId("67f530bc7d0de32c19965509") // O usar código de curso
+    }
+  },
+  {
+    $lookup: {
+      from: "students",
+      localField: "studentId",
+      foreignField: "_id",
+      as: "studentInfo"
+    }
+  },
+  {
+    $unwind: "$studentInfo"
+  },
+  {
+    $project: {
+      "studentCode": "$studentInfo.code",
+      "firstName": "$studentInfo.firstName",
+      "lastName": "$studentInfo.lastName",
+      "score": "$rate.score",
+      "email": "$studentInfo.contact.email"
+    }
+  },
+  {
+    $sort: {
+      "score": -1
+    }
+  }
+]);
+//________________________________________________________________________________________________________________
+
+
+db.inscriptionsDB.aggregate([
+  {
+    $lookup: {
+      from: "courses",
+      localField: "courseId",
+      foreignField: "_id",
+      as: "courseInfo"
+    }
+  },
+  {
+    $unwind: "$courseInfo"
+  },
+  {
+    $lookup: {
+      from: "students",
+      localField: "studentId",
+      foreignField: "_id",
+      as: "studentInfo"
+    }
+  },
+  {
+    $unwind: "$studentInfo"
+  },
+  {
+    $match: {
+      "rate.score": { $exists: true }
+    }
+  },
+  {
+    $sort: {
+      "courseInfo.code": 1,
+      "rate.score": -1
+    }
+  },
+  {
+    $group: {
+      _id: "$courseInfo._id",
+      courseCode: { $first: "$courseInfo.code" },
+      courseName: { $first: "$courseInfo.name" },
+      topStudents: {
+        $push: {
+          studentCode: "$studentInfo.code",
+          name: { $concat: ["$studentInfo.firstName", " ", "$studentInfo.lastName"] },
+          score: "$rate.score"
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      courseCode: 1,
+      courseName: 1,
+      top5Students: { $slice: ["$topStudents", 5] }
+    }
+  }
+]);
+//________________________________________________________________________________________________________________
+
+
+db.CoursesDB.aggregate([
+  {
+    $match: {
+      "teacherIds": ObjectId("65f8d3a7c1b8e8a9d4e5f101"), // ID profesor
+      "_id": ObjectId("67f530bc7d0de32c19965509"), // ID curso
+      "dateRange.startDate": "01/10/2025" // Fecha inicio
+    }
+  },
+  {
+    $lookup: {
+      from: "enrollments",
+      localField: "_id",
+      foreignField: "courseId",
+      as: "enrollments"
+    }
+  },
+  {
+    $unwind: "$enrollments"
+  },
+  {
+    $lookup: {
+      from: "students",
+      localField: "enrollments.studentId",
+      foreignField: "_id",
+      as: "studentInfo"
+    }
+  },
+  {
+    $unwind: "$studentInfo"
+  },
+  {
+    $project: {
+      _id: 0,
+      studentCode: "$studentInfo.code",
+      firstName: "$studentInfo.firstName",
+      lastName: "$studentInfo.lastName",
+      gender: "$studentInfo.gender",
+      age: {
+        $dateDiff: {
+          startDate: { $toDate: "$studentInfo.birthdate" },
+          endDate: new Date(),
+          unit: "year"
+        }
+      },
+      email: "$studentInfo.contact.email",
+      classroom: "$classRoom.code"
+    }
+  }
+]);
+//________________________________________________________________________________________________________________
+
+
+db.CoursesDB.aggregate([
+  {
+    $lookup: {
+      from: "enrollments",
+      localField: "_id",
+      foreignField: "courseId",
+      as: "enrollments"
+    }
+  },
+  {
+    $addFields: {
+      enrolledStudents: { $size: "$enrollments" }
+    }
+  },
+  {
+    $match: {
+      $expr: { $gt: ["$enrolledStudents", "$classRoom.capacity"] }
+    }
+  },
+  {
+    $project: {
+      code: 1,
+      name: 1,
+      "classRoom.code": 1,
+      capacity: "$classRoom.capacity",
+      enrolledStudents: 1,
+      overCapacity: {
+        $subtract: ["$enrolledStudents", "$classRoom.capacity"]
+      }
+    }
+  }
+]);
+//________________________________________________________________________________________________________________
